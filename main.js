a3dc4ef263c65d7262e50beb02e3f13b3df78992
Gilblasse <gilblasse@gmail.com>
2020-02-26 00:39:41 -0500

    Done.

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+let errorMessage = document.querySelector("#modal");
+let hearts = document.querySelectorAll(".like-glyph");
 
+for (heart of hearts) {
+  heart.addEventListener("click", toggleHeart);
+}
 
+function toggleHeart(e) {
+  let currentheart = e.target;
 
-
+  mimicServerCall()
+    .then(serverMessage => {
+      if (currentheart.innerText == EMPTY_HEART) {
+        currentheart.innerText = FULL_HEART;
+        currentheart.className = "activated-heart";
+      } else {
+        currentheart.innerText = EMPTY_HEART;
+        currentheart.className = "";
+      }
+      errorMessage.setAttribute("class", "hidden");
+    })
+    .catch(error => {
+      errorMessage.setAttribute("class", "");
+    });
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
